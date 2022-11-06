4cb6b34060df381e555999fd56de21611337666c
iaeroProtocol <iaero@proton.me>
2022-11-06 13:19:00 +0000

    Update PriceContext.tsx

--- src/components/contexts/PriceContext.tsx
+++ src/components/contexts/PriceContext.tsx
@@ -48,7 +48,7 @@ export interface PriceContextValue {
 }
 
 // ---------- Defaults ----------
-const DEFAULT_UPDATE_INTERVAL = 30000; // 30 seconds
+const DEFAULT_UPDATE_INTERVAL = 300000; // 30 seconds
 
 const MOCKS: PriceData = {
   AERO: { usd: 1.1, lastUpdated: Date.now() },
